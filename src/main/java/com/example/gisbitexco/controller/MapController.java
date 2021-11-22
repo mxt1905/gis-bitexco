892eb0e15baba04015e2a42a3d5d4d9e3a4d6e03
Mai Xuan Tu <xuant.mai@m.citynow.vn>
2021-11-22 12:53:29 +0700

    Refactor: Remove comments and redundant code

--- src/main/java/com/example/gisbitexco/controller/MapController.java
+++ src/main/java/com/example/gisbitexco/controller/MapController.java
@@ -37,6 +37,7 @@ public class MapController {
 
     @GetMapping("/api/bitexco_json")
     @ResponseBody
+    @SuppressWarnings("unchecked")
     public List<JSONObject> getBitexco9() throws ParseException {
         List<JSONObject> jsonList = new ArrayList<>();
 
@@ -100,11 +101,12 @@ public class MapController {
 
     @GetMapping("/api/bitexco.geojson")
     @ResponseBody
+    @SuppressWarnings("unchecked")
     public JSONObject getBitexcoGeoJson() {
         BodyDto body = bodyService.searchById(1);
         JSONObject featureCollection = new JSONObject();
         JSONArray features = new JSONArray();
-        List<JSONArray> list = new ArrayList<JSONArray>();
+        List<JSONArray> list = new ArrayList<>();
         for (int i = 97; i <= 165; i++) {
             JSONArray coordinates = new JSONArray();
             FaceDto faceDto = faceService.searchById(i);
@@ -122,7 +124,7 @@ public class MapController {
 
             JSONObject properties = new JSONObject();
             int a = i - 97;
-            properties.put("Building name", "Tầng " + a + "Bitexco Financial");
+            properties.put("Building name", "Floor " + a + "Bitexco Financial");
             properties.put("Height", body.getBodyHeight());
             properties.put("Color", body.getBodyColor());
 
@@ -140,23 +142,13 @@ public class MapController {
 
     @GetMapping("/api/bitexco_khung1.geojson")
     @ResponseBody
-    public JSONObject getBitexcoGeoJson2() throws ParseException {
+    @SuppressWarnings("unchecked")
+    public JSONObject getBitexcoGeoJson2() {
         BodyDto body = bodyService.searchById(1);
         JSONObject featureCollection = new JSONObject();
         JSONArray features = new JSONArray();
         for (int i = 80; i <= 95; i++) {
-            //            JSONArray coordinates = new JSONArray();
-            //            List<FaceNode> faceNodeList = new ArrayList<FaceNode>();
-            //            faceNodeList = mapDao.getFaceNodeList(i);
-            //            List<JSONArray> list = new ArrayList<JSONArray>();
-            //            for (FaceNode faceNode : faceNodeList) {
-            //                JSONArray JSONArrayCoord = new JSONArray();
-            //                JSONArrayCoord.add(0, faceNode.getFaceNodePK().getNode().getX());
-            //                JSONArrayCoord.add(1, faceNode.getFaceNodePK().getNode().getY());
-            //                JSONArrayCoord.add(2, faceNode.getFaceNodePK().getNode().getZ());
-            //                list.add(JSONArrayCoord);
-            //            }
-            List<JSONArray> list = new ArrayList<JSONArray>();
+            List<JSONArray> list = new ArrayList<>();
             JSONArray coordinates = new JSONArray();
             FaceDto faceDto = faceService.searchById(i);
             faceDto.getNodes().forEach(nodeDto -> {
@@ -173,7 +165,6 @@ public class MapController {
             geometry.put("coordinates", coordinates);
 
             JSONObject properties = new JSONObject();
-            int a = i - 97;
             properties.put("Name", "Khung Bitexco Financial");
             properties.put("Height", body.getBodyHeight());
             properties.put("color", body.getBodyColor());
@@ -193,13 +184,14 @@ public class MapController {
 
     @GetMapping("/api/bitexco_hanhchinh1_geojson")
     @ResponseBody
-    public JSONObject getBitexcoGeoJson3() throws ParseException {
+    @SuppressWarnings("unchecked")
+    public JSONObject getBitexcoGeoJson3() {
         BodyDto body = bodyService.searchById(1);
         JSONObject featureCollection = new JSONObject();
         JSONArray features = new JSONArray();
         for (int i = 67; i <= 78; i++) {
             JSONArray coordinates = new JSONArray();
-            List<JSONArray> list = new ArrayList<JSONArray>();
+            List<JSONArray> list = new ArrayList<>();
             FaceDto faceDto = faceService.searchById(i);
             faceDto.getNodes().forEach(nodeDto -> {
                 JSONArray jsonArrayCoordinates = new JSONArray();
@@ -214,7 +206,6 @@ public class MapController {
             geometry.put("coordinates", coordinates);
 
             JSONObject properties = new JSONObject();
-            int a = i - 97;
             properties.put("Name", "Tòa Hành Chính Bitexco Financial");
             properties.put("Height", body.getBodyHeight());
             properties.put("color", body.getBodyColor());
@@ -234,13 +225,14 @@ public class MapController {
 
     @GetMapping("/api/bitexco_hanhchinh_geojson")
     @ResponseBody
-    public JSONObject getBitexcoGeoJson4() throws ParseException {
+    @SuppressWarnings("unchecked")
+    public JSONObject getBitexcoGeoJson4() {
         BodyDto body = bodyService.searchById(1);
         JSONObject featureCollection = new JSONObject();
         JSONArray features = new JSONArray();
         for (int i = 49; i <= 66; i++) {
             JSONArray coordinates = new JSONArray();
-            List<JSONArray> list = new ArrayList<JSONArray>();
+            List<JSONArray> list = new ArrayList<>();
             FaceDto faceDto = faceService.searchById(i);
             faceDto.getNodes().forEach(nodeDto -> {
                 JSONArray jsonArrayCoordinates = new JSONArray();
@@ -255,7 +247,6 @@ public class MapController {
             geometry.put("coordinates", coordinates);
 
             JSONObject properties = new JSONObject();
-            int a = i - 97;
             properties.put("Name", "Tòa Hành Chính Bitexco Financial");
             properties.put("Height", body.getBodyHeight());
             properties.put("color", body.getBodyColor());
@@ -275,14 +266,14 @@ public class MapController {
 
     @GetMapping("/api/bitexco_sanbay.geojson")
     @ResponseBody
-    public JSONObject getBitexcoGeoJson5() throws ParseException {
+    @SuppressWarnings("unchecked")
+    public JSONObject getBitexcoGeoJson5() {
         BodyDto body = bodyService.searchById(1);
-        ;
         JSONObject featureCollection = new JSONObject();
         JSONArray features = new JSONArray();
         for (int i = 96; i <= 96; i++) {
             JSONArray coordinates = new JSONArray();
-            List<JSONArray> list = new ArrayList<JSONArray>();
+            List<JSONArray> list = new ArrayList<>();
             FaceDto faceDto = faceService.searchById(i);
             faceDto.getNodes().forEach(nodeDto -> {
                 JSONArray jsonArrayCoordinates = new JSONArray();
@@ -297,7 +288,6 @@ public class MapController {
             geometry.put("coordinates", coordinates);
 
             JSONObject properties = new JSONObject();
-            int a = i - 97;
             properties.put("Name", "Tòa Hành Chính Bitexco Financial");
             properties.put("Height", body.getBodyHeight());
             properties.put("color", body.getBodyColor());
@@ -317,13 +307,13 @@ public class MapController {
 
     @GetMapping("/api/bitexco_hanhchinh_bao.geojson")
     @ResponseBody
-    public JSONObject getBitexcoGeoJson6() throws ParseException {
+    public JSONObject getBitexcoGeoJson6() {
         BodyDto body = bodyService.searchById(1);
         JSONObject featureCollection = new JSONObject();
         JSONArray features = new JSONArray();
         for (int i = 47; i <= 48; i++) {
             JSONArray coordinates = new JSONArray();
-            List<JSONArray> list = new ArrayList<JSONArray>();
+            List<JSONArray> list = new ArrayList<>();
             FaceDto faceDto = faceService.searchById(i);
             faceDto.getNodes().forEach(nodeDto -> {
                 JSONArray jsonArrayCoordinates = new JSONArray();
@@ -338,7 +328,6 @@ public class MapController {
             geometry.put("coordinates", coordinates);
 
             JSONObject properties = new JSONObject();
-            int a = i - 97;
             properties.put("Name", "Tòa Hành Chính Bitexco Financial");
             properties.put("Height", body.getBodyHeight());
             properties.put("color", body.getBodyColor());
