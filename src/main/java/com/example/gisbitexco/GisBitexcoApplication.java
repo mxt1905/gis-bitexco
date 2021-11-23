7cf7446119d5af0ee9a6edad9e9825c8e01e4ce9
Mai Xuan Tu <xuant.mai@m.citynow.vn>
2021-11-23 10:16:42 +0700

    Refactor: Move js and css to static

--- src/main/java/com/example/gisbitexco/GisBitexcoApplication.java
+++ src/main/java/com/example/gisbitexco/GisBitexcoApplication.java
@@ -3,6 +3,9 @@ package com.example.gisbitexco;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * GisBitexcoApplication
+ */
 @SpringBootApplication
 public class GisBitexcoApplication {
 
